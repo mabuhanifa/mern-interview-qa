9eba0b74bb112de5f3a957f091996eda577165fb
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-14 15:04:08 +0600

    html

---
+++ html/html.js
@@ -0,0 +1,98 @@
+/*
+Sure! Here are 25 commonly asked HTML interview questions along with their answers:
+
+1. What is HTML?
+HTML stands for HyperText Markup Language. It is the standard markup language used for creating web pages and describing their structure.
+
+2. What are the new features introduced in HTML5?
+HTML5 introduced several new features, including semantic elements (header, footer, section, article, etc.), audio and video elements, canvas for drawing graphics, local storage, geolocation, and more.
+
+3. What is the purpose of the doctype declaration in HTML?
+The doctype declaration is used to specify the version of HTML that the web page is written in. It helps browsers to interpret the markup correctly.
+
+4. What is the difference between HTML elements and tags?
+HTML elements define the structure and content of a web page, while HTML tags are used to mark up elements and define how they should be displayed.
+
+5. What is the difference between span and div?
+The `<span>` element is an inline element used for grouping and applying styles to a small piece of text, while the `<div>` element is a block-level element used for grouping larger sections of content or defining layout regions.
+
+6. What is the purpose of the alt attribute in the `<img>` tag?
+The `alt` attribute in the `<img>` tag is used to provide alternative text for an image if it cannot be displayed. It is also used by screen readers for accessibility purposes.
+
+7. What is the difference between `<ol>` and `<ul>`?
+`<ol>` represents an ordered (numbered) list, while `<ul>` represents an unordered (bullet) list.
+
+8. What is the purpose of the `<a>` tag in HTML?
+The `<a>` tag is used to create hyperlinks. It defines a link to another webpage, a file, or a specific location within the same webpage.
+
+9. What is the role of the `<head>` tag in HTML?
+The `<head>` tag is used to contain meta-information about the HTML document, such as the title of the page, links to external stylesheets or scripts, and other metadata.
+
+10. What is the difference between `<section>` and `<div>`?
+The `<section>` element represents a standalone section of content, while the `<div>` element is a generic container used for grouping content without conveying any specific meaning.
+
+11. What is the purpose of the `rowspan` and `colspan` attributes in HTML tables?
+The `rowspan` attribute specifies the number of rows a table cell should span, while the `colspan` attribute specifies the number of columns it should span.
+
+12. How do you include CSS styles in an HTML document?
+CSS styles can be included in an HTML document by using the `<style>` tag within the `<head>` section or by linking an external CSS file using the `<link>` tag.
+
+13. What is the purpose of the `<form>` tag in HTML?
+The `<form>` tag is used to create an HTML form that allows users to input data. It can be used to submit data to a server for processing.
+
+14. What is the difference between a `<button>` and an `<input type="submit">`?
+Both `<button>` and `<input type="submit">` elements are used to create buttons that can trigger actions. However, `<button>` allows more flexibility in terms of styling and content, while `<input type="submit">` is specifically designed for submitting forms.
+
+15. What is the purpose of the `<iframe>` tag?
+The `<iframe>` tag is used to embed another HTML document or webpage within the current document. It is commonly used to display content from external sources, such as videos or maps.
+
+16. What are semantic elements in HTML5?
+Semantic elements in HTML5 are elements that carry meaning. They provide a clearer structure and convey the intended purpose of the content, making it easier for search engines and assistive technologies to understand the web page.
+
+17. What is the purpose of the `<header>` and `<footer>` tags?
+The `<header>` tag is used to define the header section of a document or a section, typically containing the title, logo, and introductory content. The `<footer>` tag represents the footer section, often containing copyright information, links to related documents, or contact details.
+
+18. How do you create a hyperlink that opens in a new tab or window?
+To create a hyperlink that opens in a new tab or window, you can use the `target="_blank"` attribute in the `<a>` tag, like this: `<a href="https://example.com" target="_blank">Link</a>`.
+
+19. What is the purpose of the `required` attribute in form input fields?
+The `required` attribute is used to specify that an input field must be filled out before submitting the form. It helps ensure that important information is not omitted.
+
+20. How can you add comments in HTML?
+Comments in HTML can be added using the `<!-- -->` syntax. Anything between these comment tags will be ignored by the browser and is only visible in the HTML source code.
+
+21. What is the purpose of the `target` attribute in the `<a>` tag?
+The `target` attribute is used to specify where the linked document should be opened when the user clicks on the link. It can be set to `_blank` to open the link in a new tab or window, or to a specific frame or window name.
+
+22. What is the purpose of the `<nav>` tag?
+The `<nav>` tag is used to define a section of navigation links on a webpage. It typically contains links to different pages or sections within the same page.
+
+23. How can you embed a video in HTML?
+You can embed a video in HTML by using the `<video>` tag and specifying the video source using the `<source>` tag within it. Example: 
+```
+<video controls>
+  <source src="video.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+```
+
+24. How do you create a table in HTML?
+Tables can be created in HTML using the `<table>` tag, and rows and cells are defined using the `<tr>` and `<td>` tags, respectively. Example:
+```
+<table>
+  <tr>
+    <td>Row 1, Cell 1</td>
+    <td>Row 1, Cell 2</td>
+  </tr>
+  <tr>
+    <td>Row 2, Cell 1</td>
+    <td>Row 2, Cell 2</td>
+  </tr>
+</table>
+```
+
+25. What is the purpose of the `placeholder` attribute in form input fields?
+The `placeholder` attribute is used to provide a short hint or example value inside an input field before the user enters their own value. It serves as a visual aid or prompt to guide the user on what information is expected.
+
+These are just a few examples of HTML interview questions you may come across. It's always a good idea to study and understand the fundamentals of HTML to be well-prepared for an interview.
+*/
